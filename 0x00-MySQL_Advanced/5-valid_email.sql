8a9eac3b46fa1f10e2bc7205864b58e7f37e6f0e
birhanus <birhanushimles716@gmail.com>
2022-08-13 22:52:03 +0000

    fix

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -2,14 +2,14 @@
 -- valid_email only when email has been changed
 
 DROP TRIGGER IF EXISTS check_valid_email
+
 DELIMITER $$
 
 CREATE TRIGGER  check_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
   BEGIN
-    IF OLD.email != NEW.email
-    THEN
+    IF OLD.email != NEW.email THEN
       SET NEW.valid_email = 0;
     ELSE
       SET NEW.valid_email = NEW.valid_email;
