2b1525128c12149a5e2ccb6206e39436e3a68c55
birhanus <birhanushimles716@gmail.com>
2022-08-13 22:15:37 +0000

    Email validation to sent

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,20 @@
+-- SQL script that creates a trigger that resets the attribure valid_email
+-- only when email has been changed
+
+DELIMITER $$
+
+CREATE TRIGGER IF NOT EXISTS check_valid_email
+  AFTER UPDATE ON users
+  FOR EACH ROW
+  BEGIN
+    IF NEW.email != OLD.email
+    THEN
+      SET NEW.valid_email = 0;
+    ELSE
+      SET NEW.valid_email = NEW.valid_email;
+    END IF;
+  END$$
+
+DELIMITER ;
+
+
